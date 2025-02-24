5583f27b8f9ad076e0f387f6a9533f78c0c19bd2
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-24 20:17:08 +0200

    done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 import pkg from 'jsonwebtoken'
 import { JWT_KEY } from "../config/env.js";
 import User from "../models/UserModel.js";
+import { compare } from 'bcrypt';
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 const { sign } = pkg;
@@ -32,5 +33,45 @@ export const signUp = async (req, res, next) => {
         console.log({ error });
         return res.status(500).send("Internal server error");
     }
-}
+};
+
+
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send("Email and Password is required");
+        };
+
+        const user = await User.create({ email, password });
+        if (!user) {
+            return res.status(404).send("User with this email is not found");
+        };
+
+        const auth = await compare(password, user.password);
+        if(!auth) {
+            return res.status(400).send("Password is incorrect");
+        };
+
+        res.cookie("jwt", createToken(email, user.id), {
+            maxAge,
+            secure: true,
+            sameSite: "None",
+        });
+
+        return res.status(200).json({ user: {
+            id: user.id,
+            email: user.email,
+            profileSetup: user.profileSetup,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            image: user.image,
+            color: user.color,
+        }});
+    } catch (error) {
+        console.log({ error });
+        return res.status(500).send("Internal server error");
+    }
+};
+
 
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signUp } from '../controllers/AuthController.js';
+import { signIn, signUp } from '../controllers/AuthController.js';
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
-//authRouter.post('/sign-in', "signIn");
+authRouter.post('/sign-in', signIn);
 //authRouter.post('/sign-out', "signOut");
 
 export default authRouter;
