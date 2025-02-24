7f1755d3555c8ea4a0cf100894bca663cc1b0f87
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-24 15:41:31 +0200

    auth model added

---
+++ models/UserModel.js
@@ -0,0 +1,53 @@
+import { genSalt } from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is Required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is Required"],
+        minLength: 6,
+    },
+    firstName: {
+        type: String,
+        trim: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    image: {
+        type: String,
+        trim: true,
+    },
+    color: {
+        type: Number,
+    },
+    profileSetup: {
+        type: Boolean,
+        default: false,
+    }
+},  { timestamps: true });
+
+
+//password incryption
+userSchema.pre("save", async function(next){
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt);
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
