f029cd7a0e3f8b233fa2cefc31a6bd8fb8cf0733
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-25 15:40:38 +0200

    +

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -5,6 +5,6 @@ const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post('/sign-in', signIn);
-//authRouter.post('/sign-out', "signOut");
+authRouter.get('/user-info', getUserInfo);
 
 export default authRouter;
