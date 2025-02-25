5d620d598285ef85e8e3654ebeec4887767ff943
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-25 16:14:26 +0200

    done

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signIn, signUp } from '../controllers/AuthController.js';
+import { getUserInfo, signIn, signUp } from '../controllers/AuthController.js';
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post('/sign-in', signIn);
-authRouter.get('/user-info', getUserInfo);
+authRouter.get('/user-info', verifyToken, getUserInfo);
 
 export default authRouter;
