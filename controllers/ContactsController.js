32251b27280f2a1d4c838695ebe3a6ea4d1e45db
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-03-05 18:59:24 +0200

    done

--- controllers/ContactsController.js
+++ controllers/ContactsController.js
@@ -1,3 +1,4 @@
+import Message from "../models/MessagesModel.js";
 import User from "../models/UserModel.js";
 
 
@@ -24,6 +25,67 @@ export const searchContacts = async (req, res, next) => {
             }
         ],
     });
+
+    return res.status(200).json({ contacts });
+    } catch (error) {
+        console.log({ error });
+        return res.status(500).send("Internal server error");
+    }
+};
+
+
+export const getContactsForDMList = async (req, res, next) => {
+    try {
+        let {userId} = req;
+        userId = new mongoose.Types.ObjectId(userId);
+
+        const contacts = await Message.aggregate([
+            {
+                $match: {
+                    $or: [{ sender: userId}, { recipient: userId }],
+                }
+            },
+            {
+                $sort: { timestamp: -1 }
+            },
+            { 
+                $group: {
+                _id: {
+                    $cond: {
+                        if: {$eq: [ "$sender", userId ]},
+                        then: "$recipient",
+                        else: "$sender",
+                    },
+                },
+                lastMessageTime: { $first: "$timestamp" },
+                },
+            },
+            {
+                $lookup: {
+                from: "users",
+                localField: "_id",
+                foreignField: "_id",
+                as: "contactInfo",
+                },
+            },
+            {
+                $unwind: "$contactInfo",
+            },
+            {
+                $project: {
+                    _id: 1,
+                    lastMessageTime: 1,
+                    email: "$contactInfo.email",
+                    firstName: "$contactInfo.email",
+                    lastName: "$contactInfo.email",
+                    image: "$contactInfo.email",
+                    color: "$contactInfo.color",
+                },
+            },
+            {
+                $sort: { lastMessageTime: -1 },
+            }
+        ]);
 
     return res.status(200).json({ contacts });
     } catch (error) {
--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { searchContacts } from "../controllers/ContactsController.js";
+import { getContactsForDMList, searchContacts } from "../controllers/ContactsController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const contactsRouter = Router();
 
 contactsRouter.post("/search", verifyToken, searchContacts);
+contactsRouter.get("/get-contacts-for-dm", verifyToken, getContactsForDMList);
 
 export default contactsRouter;
