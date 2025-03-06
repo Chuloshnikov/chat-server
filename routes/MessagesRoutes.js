a746e954a10643be2cbc254331ba6439c6f66fc8
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-03-06 19:18:25 +0200

    done

--- routes/MessagesRoutes.js
+++ routes/MessagesRoutes.js
@@ -4,7 +4,10 @@ import { getMessages } from "../controllers/MessagesController.js";
 
 const messagesRoutes = Router();
 
+const upload = multer({ dest: "uploads/files"});
+
 messagesRoutes.post("/get-messages", verifyToken, getMessages);
+messagesRoutes.post("/upload-file", verifyToken, upload.single("file"), uploadFile);
 
 export default messagesRoutes;
 
